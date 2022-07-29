c28d75903f2e0f045df9565ebcff9b6f9ddf0892
laurazig <lhzig@protonmail.com>
2022-07-29 17:49:55 +0200

    added twitter logo

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -31,35 +31,33 @@ export default function Contact() {
           <div className="conIcon">
             <a href="https://www.linkedin.com/in/laura-hesse-a998048a" target="_blank" rel="noreferrer">
               {/* <LinkedIn className="icon" /> */}
-              <img src="assets/TLinked2.jpg" alt="" className="icon"/>
-              <h3> Laura Hesse</h3>
+              <img src="assets/TLinked2.jpg" alt="" className="icon" />
+              {/* <h3> Laura Hesse</h3> */}
             </a>
-
-          </div>
-          <div className="conIcon">
             <a href="https://github.com/Laurazig">
               {/* <GitHub className="icon" /> */}
-              <img src="assets/githubIcon.jpg" alt="" className="icon"/>
-              <h3>Laurazig</h3>
+              <img src="assets/githubIcon.jpg" alt="" className="icon" />
+              {/* <h3>Laurazig</h3> */}
             </a>
-
+            <a href="https://github.com/Laurazig">
+              <img src="assets/twitter.png" alt="" className="icon" />
+              {/* <h3>@zukunfty</h3> */}
+            </a>
           </div>
-
-
         </div>
 
         <a href="https://www.linkedin.com/groups/12668793/" className="white">
-          
-            <div className="left">
-           
-              <p>Why not join my 'Women Who Code' group on LinkedIn or recommend it to a friend?!</p>
-            </div>
-            <div className="right">
-              {/* <LinkedIn className="icon" /> */}
-              {/* <img src="assets/Contact_LinkedInGroup.jpg" alt="LinkedIn screenshot of group" /> */}
-              <img src="assets/TLinked2.jpg" alt="" />
-            </div>
-          
+
+          <div className="left">
+
+            <p>Why not join my 'Women Who Code' group on LinkedIn or recommend it to a friend?!</p>
+          </div>
+          <div className="right">
+            {/* <LinkedIn className="icon" /> */}
+            {/* <img src="assets/Contact_LinkedInGroup.jpg" alt="LinkedIn screenshot of group" /> */}
+            <img src="assets/TLinked2.jpg" alt="" />
+          </div>
+
         </a>
 
         {/* <form onSubmit={handleSubmit}>
