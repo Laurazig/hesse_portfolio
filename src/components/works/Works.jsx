c688727ea2c188acc55a8028c32c54f665993dd8
laurazig <lhzig@protonmail.com>
2022-08-21 21:13:45 +0200

    wanting to add SPA wardrobe to works page but can only have 3 projects

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -13,12 +13,10 @@ export default function Works() {
       desc: "Under construction: In a team of 4, I have the role of SCRUM master. E-commerce site- vegan organic version of Hello Fresh. To be presented on Sept 6th. ",
       img: "assets/bioBites.jpg",
       link: "https://clone-dci-final.vercel.app/",
-      // tech: ["react"]
       tech: "Frontend: SPA React.js. | backend: to be deployed  - MongoDB"
     },
     {
       id: "2",
-      // icon: <Language />,
       title: "DCI Jam Hackathon",
       desc: "2 WebDev students, 2 Online Marketing students, 3 gruling days, 1 amazing experience. June 2022",
       img: "assets/W_HackathonShort.jpg",
@@ -26,8 +24,16 @@ export default function Works() {
       //tech: ["SCSS", "Bootstrap"]
       tech: "SCSS, Bootstrap"
     },
+    // {
+    //   id: "3",
+    //   title: "SPA Leipzig Wardrobe",
+    //   desc: "Add a workshop to the list",
+    //   img: "assets/W_react_Wardrobe_black.jpg",
+    //   link: "https://leipzig-wardrobe-react.vercel.app/",
+    //   tech: "React, CSS"
+    // },
     {
-      id: "3",
+      id: "4",
       // icon: <Language />,
       title: "www.leipzigg.com",
       desc: "A website I created to inform & build bridges in the local community. Hosted with Azure. 2021",
@@ -44,7 +50,6 @@ export default function Works() {
     //   link: "https://hesse-final-project2.herokuapp.com",
     //   tech: "MERN stack, Express validation, JWT, cookies, Atlas, MVC, Heroku."
     // },
-
   ]
   const handleClick = (way) => {
     way === "left" ? setCurrentSlider(currentSlider > 0 ? currentSlider - 1 : 2) :
@@ -52,39 +57,28 @@ export default function Works() {
   }
   return (
     <div className="works" id="works">
-      {/* <h1>projects</h1> */}
       <div className="slider" style={{ transform: `translateX(-${currentSlider * 100}vw)` }}>
         {data.map((d) => (
           <div className="container">
             <div className="arrowL" onClick={() => handleClick("left")}>◄</div>
-            {/* <ArrowBackIos className="arrow left" onClick={()=>handleClick("left")} /> */}
             <a href={d.link} target="_blank" rel="noreferrer">
               <div className="item">
                 <div className="left">
                   <div className="leftContainer">
-                    {/* <div className="imgContainer">
-                     <img src={d.img} alt="" /> 
-                    <MobileScreenShare className="icon" /> 
-                  </div> */}
-
                     <h2>{d.title}</h2>
                     <p>{d.desc}</p>
                     <span>{d.tech}</span>
                   </div>
                 </div>
                 <div className="right">
-
                   <img src={d.img} alt="images of websites " />
                 </div>
               </div>
             </a>
             <div className="arrowR" onClick={() => handleClick("left")}>►</div>
-            {/* <ArrowForwardIos className="arrow right"  onClick={()=>handleClick("left")} /> */}
           </div>
         ))}
       </div>
-
-
     </div>
   )
 }
